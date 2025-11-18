d16ad55c0bbbce22348a77cffa424ee343df795b
programmerwellington <wellington.a.ayala@gmail.com>
2025-11-18 14:21:22 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -9,6 +9,7 @@ import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import com.educandoweb.course.services.exceptions.DatabaseException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -41,9 +42,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	private void updateData(User entity, User obj) {
